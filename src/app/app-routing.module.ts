325ba4c97b7f720d260333cca1f590e2e759985e
LucasJoelMcG <93175256+LucasJoelMcG@users.noreply.github.com>
2023-03-30 16:08:50 -0300

    Component clean-up and re-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
-//import { CommonModule } from '@angular/common';
-import { Router, RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-import { IntroComponent } from './components/intro/intro.component';
+import { RouterModule, Routes } from '@angular/router';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
 const routes:Routes = [
-  { path: 'intro', component: IntroComponent, pathMatch: "full" },
   { path: 'portfolio', component: PortfolioComponent, pathMatch: "full"},
-  { path: '', component: IntroComponent },
-  { path: '**', component: IntroComponent}
+  { path: '', component: PortfolioComponent },
+  { path: '**', component: PortfolioComponent}
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { IntroComponent } from './components/intro/intro.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @NgModule({
   declarations: [
     AppComponent,
-    IntroComponent,
     PortfolioComponent
   ],
   imports: [
